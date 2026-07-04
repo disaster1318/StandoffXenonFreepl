f9d42ff47e6dd7ac75ed22eb4943ca7010523705
disaster1318 <kirillkazancev160@gmail.com>
2026-07-05 03:29:40 +0700

    Create sdk.h

---
+++ jni/src/sdk/sdk.h
@@ -0,0 +1,12 @@
+#ifndef SDK_H
+#define SDK_H
+
+#include "il2cpp/il2cpp_structs.h"
+
+// Типы для IL2CPP API
+using assembly = Il2CppAssembly;
+using klass = Il2CppClass;
+using image = Il2CppImage;
+using method_info = MethodInfo;
+
+#endif
